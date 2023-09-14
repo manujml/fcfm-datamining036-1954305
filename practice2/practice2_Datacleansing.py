c42a54ce2f27f6123c9496523b3e5446146baf85
manujml <manuel.juarezmelchor@gmail.com>
2023-09-14 00:00:34 -0600

    updating csv and practice2

--- practice2/practice2_Datacleansing.py
+++ practice2/practice2_Datacleansing.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import os
 
-df = pd.read_csv('C:/Users/Admin/Documents/Documentos Manuel/0. UANL/FCFM/7mo SEMESTRE/Minería de Datos/SISMOS_MEX_1975-2023.csv')
+# Cargar el archivo CSV en un DataFrame
+df = pd.read_csv('C:/Users/Admin/Documents/Documentos Manuel/0. UANL/FCFM/7mo SEMESTRE/Minería de Datos/csv/SISMOS_MEX_1975-2023.csv')
 
 # Borra todas las cadenas antes de la cadena 'de ', incluyendo a dicha cadena
 df['Referencia de localizacion'] = df['Referencia de localizacion'].str.split('de ').str[1].str.strip()
@@ -10,11 +12,8 @@ new_name = 'Localizacion'
 df = df.rename(columns={'Referencia de localizacion': new_name})
 
 # Elimina las filas que incluyan 'no calculable' en la columna 'Magnitud'
-df = df[df['Magnitud'] != 'no calculable']
-
-# Eliminando columnas 'Latitud' y 'Longitud'
-delete_col = ['Latitud', 'Longitud']
-df = df.drop(delete_col, axis=1)
+dlte_rows = df[df['Magnitud'] == 'no calculable'].index
+df.drop(dlte_rows, inplace=True)
 
 # Guarda el nuevo csv con los datos limpios
 df.to_csv('SISMOS_MEX_Practice2.csv', index=False)
