41b3660c8bf90c17d40a0e9fdf2d146e86cb5724
manujml <manuel.juarezmelchor@gmail.com>
2023-09-14 00:05:52 -0600

    update practice2

--- practice2/practice2_Datacleansing.py
+++ practice2/practice2_Datacleansing.py
@@ -1,17 +1,18 @@
 import pandas as pd
 import os
 
-# Cargar el archivo CSV en un DataFrame
+# Leyendo el archivo csv
 df = pd.read_csv('C:/Users/Admin/Documents/Documentos Manuel/0. UANL/FCFM/7mo SEMESTRE/Minería de Datos/csv/SISMOS_MEX_1975-2023.csv')
+                    # ↑ cambiar la dirección en donde se encuentra el archivo localmente en el dispositivo
 
-# Borra todas las cadenas antes de la cadena 'de ', incluyendo a dicha cadena
+# Borrar todas las cadenas antes de la cadena 'de ', incluyendo a dicha cadena
 df['Referencia de localizacion'] = df['Referencia de localizacion'].str.split('de ').str[1].str.strip()
 
 # Cambiando el nombre de la columna 'Referencia de localizacion'
 new_name = 'Localizacion'
 df = df.rename(columns={'Referencia de localizacion': new_name})
 
-# Elimina las filas que incluyan 'no calculable' en la columna 'Magnitud'
+# Elimina las filas que incluyan 'no calculable' en la columna 'Magnitud', puesto que no se puede trabajar con dichos datos
 dlte_rows = df[df['Magnitud'] == 'no calculable'].index
 df.drop(dlte_rows, inplace=True)
 
