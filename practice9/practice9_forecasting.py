0a4abd52ca8e87ca877aeb6e91bd84915d4f0e7d
manujml <manuel.juarezmelchor@gmail.com>
2023-11-07 03:06:21 -0600

    practice 9 | forecasting

--- practice9/practice9_forecasting.py
+++ practice9/practice9_forecasting.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
-# Cargar el archivo CSV
+# archivo CSV local
 df = pd.read_csv('C:/Users/Admin/Documents/Documentos Manuel/0. UANL/FCFM/7mo SEMESTRE/Minería de Datos/csv/SISMOS_MEX_v2.csv')
 
 # Convertir la columna 'Fecha' a tipo datetime por si las dudas xd
